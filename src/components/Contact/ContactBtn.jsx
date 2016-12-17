ea34de312a1edc9f2e1a7d7bd85516b0463e1e8b
Mark Lyck <mark.lyck@gmail.com>
2016-12-17 19:50:10 +0100

    Hero updates

--- src/components/Contact/ContactBtn.jsx
+++ src/components/Contact/ContactBtn.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import './contact.css'
 
 class ContactBtn extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <button className="contact-btn">{this.props.text}</button>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
+import $ from 'jquery'
 import logo from '../../../public/images/logo.svg'
 import './hero.css'
 import ContactBtn from '../Contact/ContactBtn'
+import '../../libraries/typed.js'
 
 class Hero extends React.Component {
+  componentDidMount() {
+    $("#build").typed({
+        stringsElement: $('#builds'),
+        typeSpeed: 0,
+        loop: true,
+        showCursor: true,
+        cursorChar: "|",
+        backDelay: 1500,
+        backSpeed: 0,
+    })
+  }
+
   render() {
     return (
       <div className="hero">
-        <img className="logo" src={logo} alt="logo"/>
-        <h1 className="title">Hi! <span className="thin">I'm</span> Mark. <span className="thin">I design & build</span></h1>
-        <h1>Landing Pages.</h1>
-        <ContactBtn text="Yes, I'm available for hire"/>
-        <div className="learn-more">
-          <p>Learn more about what I do</p>
-          <i className="fa fa-chevron-down" aria-hidden="true"></i>
-        </div>
+          <img className="logo" src={logo} alt="logo"/>
+          <h1 className="title">Hi! <span className="thin">I'm</span> Mark. <span className="thin">I design & build</span></h1>
+          <div className="typed">
+            <h1 id="build"></h1>
+            <div id="builds">
+              <h1>landing pages.</h1>
+              <h1>user interfaces.</h1>
+              <h1>web applications.</h1>
+              <h1>neural networks.</h1>
+              <h1>Corporate Websites.</h1>
+            </div>
+          </div>
+          <ContactBtn text="Yes, I'm available for hire"/>
+          <div className="learn-more">
+            <p>Learn more about what I do</p>
+            <i className="fa fa-chevron-down" aria-hidden="true"></i>
+          </div>
       </div>
     )
   }
