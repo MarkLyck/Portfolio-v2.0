55684c8949a5408cfad304687acf31fc050682c4
Mark Lyck <mark.lyck@gmail.com>
2016-12-20 01:38:13 +0100

    Contact module

--- src/components/Contact/ContactBtn.jsx
+++ src/components/Contact/ContactBtn.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
+import logo from '../../../public/images/logo.svg'
 import './contact.css'
 
 class ContactBtn extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.toggleModal = this.toggleModal.bind(this)
+
+    this.state = { showModal: false }
+  }
+
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal })
+  }
+
   render() {
-    return (
-      <button className="contact-btn">{this.props.text}</button>
-    )
+    if (!this.state.showModal) {
+      return <button className="contact-btn" onClick={this.toggleModal}>{this.props.text}</button>
+    } else {
+      return (
+        <div className="contact">
+          <div className="contact-modal">
+            <button className="close-btn" onClick={this.toggleModal}></button>
+            <img className="logo" src={logo} alt="logo"/>
+            <h2>Ok, just a few quick questions.</h2>
+            <h3>After you introduce yourself and your project,
+                I'll get it touch with you to schedule a time to chat.
+                You should expect to hear from me in a day or so.
+            </h3>
+            <form>
+              <div className="name-email">
+                <input type="text" placeholder="First Name"/>
+                <input type="email" placeholder="email"/>
+              </div>
+              <div className="phone-website">
+                <input type="text" placeholder="Phone Number"/>
+                <input type="text" placeholder="Website"/>
+              </div>
+              <textarea />
+              <input type="submit" className="submit" value="Submit your project"/>
+            </form>
+          </div>
+          <div className="modal-container"/>
+          <button className="contact-btn" onClick={this.toggleModal}>{this.props.text}</button>
+        </div>)
+    }
+
   }
 }
 
