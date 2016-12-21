836df236da01d74e8bb7f7851d76125134e78bba
Mark Lyck <mark.lyck@gmail.com>
2016-12-21 19:05:28 +0100

    Contact button link functional

--- src/components/Contact/ContactBtn.jsx
+++ src/components/Contact/ContactBtn.jsx
@@ -1,54 +1,26 @@
 import React from 'react'
-import logo from '../../../public/images/logo.svg'
+import { browserHistory } from 'react-router'
 import './contact.css'
-import $ from 'jquery'
 
 class ContactBtn extends React.Component {
   constructor(props) {
     super(props)
 
-    this.toggleModal = this.toggleModal.bind(this)
+    this.goToContact = this.goToContact.bind(this)
 
-    this.state = { showModal: false }
+    this.state = { goToContact: false }
   }
 
-  toggleModal() {
-    this.setState({ showModal: !this.state.showModal })
+  goToContact() {
+    this.setState({ goToContact: true })
+    window.setTimeout(() => { browserHistory.push('/contact') }, 1500)
   }
 
   render() {
-    if (!this.state.showModal) {
-      return <button className="contact-btn" onClick={this.toggleModal}>{this.props.text}</button>
+    if (!this.state.goToContact) {
+      return <button className="contact-btn" onClick={this.goToContact}>{this.props.text}</button>
     } else {
-      return (
-        <div className="contact">
-
-          <div className="contact-modal">
-            <button className="close-btn" onClick={this.toggleModal}></button>
-            <img className="logo" src={logo} alt="logo"/>
-            <h2>Ok, just a few quick questions.</h2>
-            <h3>After you introduce yourself and your project,
-                I'll get it touch with you to schedule a time to chat.
-                You should expect to hear from me in a day or so.
-            </h3>
-            <form>
-              <div className="name-email">
-                <input type="text" placeholder="First Name"/>
-                <input type="email" placeholder="email"/>
-              </div>
-              <div className="phone-website">
-                <input type="text" placeholder="Phone Number"/>
-                <input type="text" placeholder="Website"/>
-              </div>
-              <textarea placeholder="Tell me about your project... What is it? Why are you doing it? What do you hope to accomplish? How can I help? Timeline and budget details are also appreciated." />
-              <input type="submit" className="submit" value="Submit your project"/>
-            </form>
-          </div>
-
-          <div className="modal-container"/>
-
-          <button className="contact-btn circle" onClick={this.toggleModal}>{this.props.text}</button>
-        </div>)
+      return <button className="contact-btn circle">{this.props.text}</button>
     }
 
   }
--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import logo from '../../../public/images/logo.svg'
 import './contactpage.css'
 
@@ -6,25 +7,27 @@ class ContactPage extends React.Component {
   render() {
     return (
       <div className="contact-page">
-        <button className="close-btn" onClick={this.toggleModal}></button>
-        <img className="logo" src={logo} alt="logo"/>
-        <h2>Ok, just a few quick questions.</h2>
-        <h3>After you introduce yourself and your project,
-            I'll get it touch with you to schedule a time to chat.
-            You should expect to hear from me in a day or so.
-        </h3>
-        <form>
-          <div className="name-email">
-            <input type="text" placeholder="First Name"/>
-            <input type="email" placeholder="email"/>
-          </div>
-          <div className="phone-website">
-            <input type="text" placeholder="Phone Number"/>
-            <input type="text" placeholder="Website"/>
-          </div>
-          <textarea placeholder="Tell me about your project... What is it? Why are you doing it? What do you hope to accomplish? How can I help? Timeline and budget details are also appreciated." />
-          <input type="submit" className="submit" value="Submit your project"/>
-        </form>
+        <div className="container">
+          <Link className="close-btn" to="/"/>
+          <img className="logo" src={logo} alt="logo"/>
+          <h2>Ok, just a few quick questions.</h2>
+          <h3>After you introduce yourself and your project,
+              I'll get it touch with you to schedule a time to chat.
+              You should expect to hear from me in a day or so.
+          </h3>
+          <form>
+            <div className="name-email">
+              <input type="text" placeholder="First Name"/>
+              <input type="email" placeholder="email"/>
+            </div>
+            <div className="phone-website">
+              <input type="text" placeholder="Phone Number"/>
+              <input type="text" placeholder="Website"/>
+            </div>
+            <textarea placeholder="Tell me about your project... What is it? Why are you doing it? What do you hope to accomplish? How can I help? Timeline and budget details are also appreciated." />
+            <input type="submit" className="submit" value="Submit your project"/>
+          </form>
+        </div>
       </div>
     )
   }
