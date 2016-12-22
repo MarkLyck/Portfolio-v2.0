b215966eb3ab88b669d4c7122733ff55289f1542
Mark Lyck <mark.lyck@gmail.com>
2016-12-22 01:35:22 +0100

    docs

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -11,12 +11,18 @@ class ContactPage extends React.Component {
 
     this.sendEmail = this.sendEmail.bind(this)
     this.renderBtn = this.renderBtn.bind(this)
-    this.state = { state: 'submit' }
+    this.renderErrors = this.renderErrors.bind(this)
+
+    this.state = { state: 'submit', error: '' }
   }
 
   sendEmail(e) {
     e.preventDefault()
     if (this.state.state === 'submit') {
+      if (!this.refs.text.value || !this.refs.email.value || this.refs.name.value) {
+        this.setState({ error: 'Please fill out all required fields' })
+        return null
+      }
       this.setState({ state: 'submitting' })
       const basicAuth = btoa('kid_BkCLw_dNg:59542cf7ab86454591d5966e7c7c80b1')
       $.ajax({
@@ -56,6 +62,12 @@ class ContactPage extends React.Component {
     }
   }
 
+  renderErrors() {
+    if (this.state.error) {
+      return <p className="error"><i className="fa fa-exclamation" aria-hidden="true"></i> {this.state.error}</p>
+    }
+  }
+
   render() {
     return (
       <div className="contact-page">
@@ -67,16 +79,17 @@ class ContactPage extends React.Component {
               I'll get it touch with you to schedule a time to chat.
               You should expect to hear from me in a day or so.
           </h3>
+          {this.renderErrors()}
           <form onSubmit={this.sendEmail.bind(this)}>
             <div className="name-email">
-              <input type="text" placeholder="First Name" ref="name"/>
-              <input type="email" placeholder="email" ref="email"/>
+              <input type="text" placeholder="First Name*" ref="name"/>
+              <input type="email" placeholder="email*" ref="email"/>
             </div>
             <div className="phone-website">
               <input type="text" placeholder="Phone Number" ref="phone"/>
               <input type="text" placeholder="Website" ref="website"/>
             </div>
-            <textarea ref="text" placeholder="Tell me about your project... What is it? Why are you doing it? What do you hope to accomplish? How can I help? Timeline and budget details are also appreciated." />
+            <textarea ref="text" placeholder="Tell me about your project... What is it? Why are you doing it? What do you hope to accomplish? How can I help? Timeline and budget details are also appreciated.*" />
             {this.renderBtn()}
           </form>
         </div>
